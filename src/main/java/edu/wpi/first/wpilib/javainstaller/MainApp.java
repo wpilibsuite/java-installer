11954e0755a61f9b10ebda38c09deaa578d46ab5
Fredric Silberberg <fredric@fsilberberg.com>
2014-11-21 14:11:06 -0500

    Initial commit of the java installer

---
+++ src/main/java/edu/wpi/first/wpilib/javainstaller/MainApp.java
@@ -0,0 +1,16 @@
+package edu.wpi.first.wpilib.javainstaller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+    }
+}
