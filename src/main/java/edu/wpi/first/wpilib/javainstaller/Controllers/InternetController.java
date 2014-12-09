fcf1153c3f88f86c5915b59762ad2edf66705ee8
Fredric Silberberg <fredric@fsilberberg.com>
2014-12-09 14:27:28 -0500

    Added checking to see if the jre is already downloaded. If the file is found, the program will calculate the md5 hash of the downloaded jre and compare it to the saved hash. If they agree, the found jre will be used. If not, then the traditional download screen will be used.

--- src/main/java/edu/wpi/first/wpilib/javainstaller/Controllers/InternetController.java
+++ src/main/java/edu/wpi/first/wpilib/javainstaller/Controllers/InternetController.java
@@ -7,7 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
 import org.apache.logging.log4j.LogManager;
@@ -15,31 +15,61 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.SocketTimeoutException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Handles ensuring that the internet is up and running on the system before proceeding
  */
 public class InternetController extends AbstractController {
 
+    // Default name for the downloaded JRE.
+    private static final String JRE_DEFAULT_NAME = "ejdk-8u6-fcs-b23-linux-arm-vfp-sflt-12_jun_2014.tar.gz";
+    // The md5 hash of a fully downloaded jre
+    private static final String JRE_HASH = "082F08397B0D3F63844AB472B5111C8C";
+
     @FXML
     private BorderPane mainView;
 
     @FXML
-    private ImageView logoImageView;
-
-    @FXML
     private Button nextButton;
 
+    @FXML
+    private Button alreadyDownloadedButton;
+
+    @FXML
+    private Label textView;
+
     private final Logger m_logger = LogManager.getLogger();
+
+    private boolean jreDetected = false;
 
     public InternetController() {
         super("/fxml/intro_screen.fxml");
     }
 
     @FXML
-    public void handleNext() {
+    private void initialize() {
+        // Attempt to detect the JRE
+        final File jre = new File(JRE_DEFAULT_NAME);
+        // If the JRE is already downloaded and fully downloaded
+        if (jre.exists() && checkJRE(jre)) {
+            jreDetected = true;
+            alreadyDownloadedButton.setText("Redownload JRE");
+            textView.setText("The JRE was found already downloaded. If you want to proceed with the found JRE (recommended), click Next. If you would like to redownload the JRE, click Redownload JRE");
+            alreadyDownloadedButton.setOnAction((action) -> handleInternetCheck());
+            nextButton.setOnAction((action) -> sendToUntar(jre));
+        }
+    }
+
+    @FXML
+    public void handleInternetCheck() {
         // Handle the connection test on a background thread to avoid blocking the ui. If the user clicks next and can't
         // get to the internet, they have the ability to stop the program without waiting for a timeout
         nextButton.setDisable(true);
@@ -102,16 +132,53 @@ public class InternetController extends AbstractController {
         chooser.setSelectedExtensionFilter(new FileChooser.ExtensionFilter("Compressed JRE", "*.tar.gz"));
         File jre = chooser.showOpenDialog(mainView.getScene().getWindow());
         if (jre != null) {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/untar.fxml"));
-            try {
-                Parent root = loader.load();
-                UntarController controller = loader.getController();
-                controller.initialize(jre.getAbsolutePath());
-                mainView.getScene().setRoot(root);
-            } catch (IOException e) {
-                m_logger.error("Could not load the untar screen.", e);
-                MainApp.showErrorScreen(e);
+            sendToUntar(jre);
+        }
+    }
+
+    private void sendToUntar(File jre) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/untar.fxml"));
+        try {
+            Parent root = loader.load();
+            UntarController controller = loader.getController();
+            controller.initialize(jre.getAbsolutePath());
+            mainView.getScene().setRoot(root);
+        } catch (IOException e) {
+            m_logger.error("Could not load the untar screen.", e);
+            MainApp.showErrorScreen(e);
+        }
+    }
+
+    /**
+     * Computes the md5 of a found JRE to ensure that it is correctly downloaded
+     *
+     * @param jre The jre to check
+     * @return True if it passes the MD5 check
+     */
+    private boolean checkJRE(File jre) {
+        m_logger.debug("Found JRE, checking hash");
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            try (InputStream is = Files.newInputStream(Paths.get(jre.getAbsolutePath()))) {
+                DigestInputStream ds = new DigestInputStream(is, md);
+                byte[] input = new byte[1024];
+
+                // Read the stream to the end to get the md5
+                while (ds.read(input) != -1) {
+                }
+
+                byte[] hash = md.digest();
+                StringBuilder sb = new StringBuilder();
+                for (byte b : hash) {
+                    sb.append(String.format("%02X", b));
+                }
+                String hashString = sb.toString();
+                m_logger.debug("Computed hash is " + hashString + ", official hash is " + JRE_HASH);
+                return hashString.equalsIgnoreCase(JRE_HASH);
             }
+        } catch (NoSuchAlgorithmException | IOException e) {
+            m_logger.warn("Could not create md5 hash", e);
+            return false;
         }
     }
 }
